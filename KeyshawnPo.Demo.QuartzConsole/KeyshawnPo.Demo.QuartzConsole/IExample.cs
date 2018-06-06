38ed43a81f42bc38a1d0988b2a08338eb5df98a7
pu.guihong@outlook.com <pu.guihong@outlook.com>
2018-06-06 15:38:39 +0800

    任务调度示例
    NuGet添加log4net 引用，和common.logging 3.3.1引用。

---
+++ KeyshawnPo.Demo.QuartzConsole/KeyshawnPo.Demo.QuartzConsole/IExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyshawnPo.Demo.QuartzConsole
+{
+    public interface IExample
+    {
+        string Name
+        {
+            get;
+        }
+
+        void Run();
+    }
+}
