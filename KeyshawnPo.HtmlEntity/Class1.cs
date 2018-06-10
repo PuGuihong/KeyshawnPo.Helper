9818359e9854525907c04226878242fae3654cb6
pu.guihong@outlook.com <pu.guihong@outlook.com>
2018-06-10 22:55:50 +0800

    Html生成
    Html生成

---
+++ KeyshawnPo.HtmlEntity/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyshawnPo.HtmlEntity
+{
+    public class head
+    {
+        title title;
+    }
+
+    public class title
+    {
+
+    }
+
+    public class body
+    {
+
+    }
+
+    public class div
+    {
+
+    }
+}
