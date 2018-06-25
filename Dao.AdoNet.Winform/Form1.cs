496e491c71321dba51ddf7bd40f9ed567239074a
KeyshawnPo <pu.guihong@outlook.com>
2018-06-26 00:22:57 +0800

    ADO.NET 示例
    ADO.NET 示例

--- Dao.AdoNet.Winform/Form1.cs
+++ Dao.AdoNet.Winform/Form1.cs
@@ -28,37 +28,34 @@ namespace Dao.AdoNet.Winform
                 for (int i = 0; i < _ds.Tables[0].Rows.Count; i++)
                 {
                     string _rltMsg = string.Empty;
-                    string _connStr = _ds.Tables[0].Rows[i][2].ToString();
+                    string _connStr = _ds.Tables[0].Rows[i][3].ToString();
                     DbHelperSQL.connectionString = _connStr;
                     SqlConnection _dbConn = new SqlConnection();
                     bool _setRlt = DbHelperSQL.SetConn(ref _dbConn, ref _rltMsg);
                     //查出原始数据
                     if (false == _setRlt) continue;
-                    DataSet _DataSetRlt = DbHelperSQL.Query(" select * from CompactIndex");
+                    DataSet _DataSetRlt = DbHelperSQL.Query(" select * from Info2;");
 
                     DbHelperSQL.connectionString = "Data Source=.;Initial Catalog=MyDatabase;User ID=sa;Password=Z6626294@";
                     _setRlt = DbHelperSQL.SetConn(ref _dbConn, ref _rltMsg);
                     if (false == _setRlt) break;
+
+
                     //插入表数据
                     SqlDataAdapter _SqlDataAdapter = new SqlDataAdapter();
                     SqlCommandBuilder _SqlCommandBuilder = new SqlCommandBuilder(_SqlDataAdapter);
-                    string _strSqlSelect = "select * from CompactIndex";
-                    //string _strSqlInsert = "Insert Into CompactIndex([BillID]) values(@BillID)";
+                    string _strSqlSelect = "select * from Info";
+                    DataSet _dsApap = new DataSet();
                     _SqlDataAdapter.SelectCommand = new SqlCommand(_strSqlSelect, _dbConn);
-                    //_SqlDataAdapter.InsertCommand = new SqlCommand(_strSqlInsert, _dbConn);
                     _SqlDataAdapter.InsertCommand = new SqlCommand();
-                    _SqlDataAdapter.TableMappings.Add("ds", "CompactIndex");   // 设置对象名称 
-                    _SqlDataAdapter.Fill(_DataSetRlt, "CompactIndex");
+                    _SqlDataAdapter.Fill(_dsApap, "ds");
                     _SqlDataAdapter.InsertCommand = _SqlCommandBuilder.GetInsertCommand();
                     _SqlDataAdapter.DeleteCommand = _SqlCommandBuilder.GetDeleteCommand();
                     _SqlDataAdapter.UpdateCommand = _SqlCommandBuilder.GetUpdateCommand();
+                    _dsApap = _DataSetRlt.Copy();
 
-                    //foreach (DataRow item in _DataSetRlt.Tables[0].Rows)
-                    //{
-                    //    item.SetAdded();
-                    //}
+                    int _intRlt = _SqlDataAdapter.Update(_dsApap.GetChanges(), "ds");
 
-                    int _intRlt = _SqlDataAdapter.Update(_DataSetRlt, "CompactIndex");
 
                     DbHelperSQL.CloseConn(_dbConn);
                 }
