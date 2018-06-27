241a886cd6b289de995f8aa974c5a19dff254651
pu.guihong@outlook.com <pu.guihong@outlook.com>
2018-06-27 12:58:12 +0800

    ModelList转DataTable
    ModelList转DataTable

--- KeyshawnPo.Helper.Lib/Basic/DataTableHelper.cs
+++ KeyshawnPo.Helper.Lib/Basic/DataTableHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Reflection;
@@ -36,6 +37,47 @@ namespace KeyshawnPo.Helper.Lib
                 ts.Add(t);
             }
             return ts;
+        }
+
+        /// <summary>
+        /// ModelList转DataTable
+        /// </summary>
+        /// <param name="list">实体集合</param>
+        /// <returns>DataTable</returns>
+        public DataTable ModelList2DataTable(IList list)
+        {
+            if (list == null || list.Count == 0)
+            {
+                return null;
+            }
+            DataTable _dt = new DataTable();
+            try
+            {
+                //获取并指定列名
+                PropertyInfo[] _propertys = list[0].GetType().GetProperties();
+                foreach (PropertyInfo _pInfo in _propertys)
+                {
+                    _dt.Columns.Add(_pInfo.Name, _pInfo.PropertyType.GetGenericArguments().Length > 0 ? _pInfo.PropertyType.GetGenericArguments()[0] : _pInfo.PropertyType);
+                }
+
+                //逐行赋值
+                for (int i = 0; i < list.Count; i++)
+                {
+                    ArrayList tempList = new ArrayList();
+                    foreach (PropertyInfo pi in _propertys)
+                    {
+                        object obj = pi.GetValue(list[i], null);
+                        tempList.Add(obj);
+                    }
+                    object[] array = tempList.ToArray();
+                    _dt.LoadDataRow(array, true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("实体类转DataTable失败，" + ex.Message);
+            }
+            return _dt;
         }
     }
 }
