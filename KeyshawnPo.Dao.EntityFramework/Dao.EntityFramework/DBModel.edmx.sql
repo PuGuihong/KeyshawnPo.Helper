b95a6fd33f582cd4cd46de777169b377184fd7af
pu.guihong@outlook.com <pu.guihong@outlook.com>
2018-06-27 11:41:30 +0800

    公用MVC Web 组件方法示例。
    公用MVC Web 组件方法示例。

--- KeyshawnPo.Dao.EntityFramework/Dao.EntityFramework/DBModel.edmx.sql
+++ KeyshawnPo.Dao.EntityFramework/Dao.EntityFramework/DBModel.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 06/12/2018 21:58:13
--- Generated from EDMX file: D:\MyCodePro\MyLib\keyshawnpo.helper\KeyshawnPo.Helper\KeyshawnPo.Helper\KeyshawnPo.Dao.EntityFramework\Dao.EntityFramework\DBModel.edmx
+-- Date Created: 06/27/2018 10:57:56
+-- Generated from EDMX file: D:\OwnerTemp\WorkInfo\KeyshawnPo.CodeLib\KeyshawnPo.Helper\KeyshawnPo.Dao.EntityFramework\Dao.EntityFramework\DBModel.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
@@ -22,6 +22,9 @@ GO
 -- Dropping existing tables
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[MInfoSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[MInfoSet];
+GO
 IF OBJECT_ID(N'[dbo].[Tmplete]', 'U') IS NOT NULL
     DROP TABLE [dbo].[Tmplete];
 GO
@@ -41,6 +44,19 @@ CREATE TABLE [dbo].[Tmplete] (
 );
 GO
 
+-- Creating table 'MInfoSet'
+CREATE TABLE [dbo].[MInfoSet] (
+    [Id] bigint IDENTITY(1,1) NOT NULL,
+    [SourceName] nvarchar(50)  NULL,
+    [Source] nvarchar(50)  NOT NULL,
+    [IndependentSource] nvarchar(100)  NULL,
+    [SourceCreateDate] datetime  NULL,
+    [SourceCollectionDate] datetime  NOT NULL,
+    [SourceRemark] nvarchar(max)  NULL,
+    [Area] nvarchar(100)  NULL
+);
+GO
+
 -- --------------------------------------------------
 -- Creating all PRIMARY KEY constraints
 -- --------------------------------------------------
@@ -49,6 +65,12 @@ GO
 ALTER TABLE [dbo].[Tmplete]
 ADD CONSTRAINT [PK_Tmplete]
     PRIMARY KEY CLUSTERED ([ID] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'MInfoSet'
+ALTER TABLE [dbo].[MInfoSet]
+ADD CONSTRAINT [PK_MInfoSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
 -- --------------------------------------------------
